1db4b39f6d5b430dff52de3b854bfc84d9775235
Alexandre Plateau <lexplt.dev@gmail.com>
2025-09-15 19:04:57 +0200

    First commit, setting up the action

---
+++ download-stdlib.sh
@@ -0,0 +1,7 @@
+wget -q https://github.com/ArkScript-lang/std/archive/refs/heads/master.zip
+unzip -oq master.zip "std-master/*.ark" -x "std-master/tests/*"
+
+rm master.zip
+rm -rf .arkscript/lib/std/
+mv std-master/ .arkscript/lib/std/
+
---
+++ download.sh
@@ -0,0 +1,40 @@
+set -e
+
+VERSION="$1"
+OS=$(echo "$2" | cut -d- -f1)
+
+case "$OS" in
+    linux | ubuntu)
+        ASSET=linux-gcc-14
+        ;;
+    windows)
+        ASSET=windows-msvc-22
+        ;;
+    macos)
+        ASSET=macos-clang-16
+        ;;
+    *)
+        echo "Unknown operating system: '$OS'"
+        exit 1
+        ;;
+esac
+
+URL="https://api.github.com/repos/ArkScript-lang/Ark/releases/latest"
+if [[ "$VERSION" != "latest" ]]; then
+  URL="https://api.github.com/repos/ArkScript-lang/Ark/releases/tags/$VERSION"
+fi
+
+API_OUTPUT=$(curl -s "$URL")
+if [[ $(echo -ne "$API_OUTPUT" | grep "status.*404") != "" ]]; then
+  echo "Release '$VERSION' not found"
+  exit 1
+fi
+
+ASSET_PATH=$(echo -ne "$API_OUTPUT" | grep "browser_download_url.*${ASSET}.zip" | cut -d : -f 2,3 | tr -d \" | tr -d " ")
+wget -q "$ASSET_PATH"
+
+mkdir -p .arkscript
+unzip -oq "${ASSET}.zip" -d .arkscript
+rm "${ASSET}.zip"
+chmod +x ./.arkscript/arkscript*
+
